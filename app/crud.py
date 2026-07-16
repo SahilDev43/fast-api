a267127bbfa971d272ef882951b7acbb67ae46a3
SahilDev43 <dev528071@gmail.com>
2026-07-16 11:05:00 +0530

    new changes

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import Student
+from app.schemas import StudentCreate
 
 
 def create_student(db: Session, student: Student):
@@ -13,3 +14,20 @@ def get_students(db: Session):
 
 def get_students_id(db: Session, student_id: int):
     return db.query(Student).filter(Student.id == student_id).first()
+
+def update_student(db: Session, student_id: int, updated_student: StudentCreate):
+    student = (
+        db.query(Student)
+        .filter(Student.id == student_id)
+        .first()
+    )
+
+    if student is None:
+        return None
+    
+    student.name = updated_student.name
+    student.course = updated_student.course
+
+    db.commit()
+    db.refresh(student)
+    return student
--- app/routers/students.py
+++ app/routers/students.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas import StudentCreate
 from app.models import Student
+from fastapi import HTTPException
 import app.crud as crud
 
 router = APIRouter(
@@ -32,15 +33,25 @@ def create_student(
 
     return crud.create_student(db, new_student)
 
+
 @router.get("/")
 def get_students(db: Session = Depends(get_db)):
     return crud.get_students(db)
 
+
 @router.get("/{student_id}")
 def get_students_id(student_id: int, db: Session = Depends(get_db)):
-    get_id =  crud.get_students_id(db, student_id)
+    get_id = crud.get_students_id(db, student_id)
 
     if get_id is None:
-        raise HTTPException(status_code=404, details="Student not found")
-    
+        raise HTTPException(status_code=404, detail="Student not found")
+
     return get_id
+
+
+@router.put("/{student_id}")
+def update_student(student_id: int, updated_student: StudentCreate, db: Session = Depends(get_db)):
+    student = crud.update_student(db, student_id, updated_student)
+    if student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
